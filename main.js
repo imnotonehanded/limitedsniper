aa6df39e85730f8e79e63b74d4c11c2ee46d9af4
imnotonehanded <59941757+imnotonehanded@users.noreply.github.com>
2021-01-03 10:00:09 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-const request = require('request');
-
-request('https://economy.roblox.com/v1/users/530764514/currency', { json: true }, (err, res, body) => {
-    if (err) { return console.log(err); }
-    console.log(body);
-    console.log(body.explanation);
-});
+console.log("uwu");
